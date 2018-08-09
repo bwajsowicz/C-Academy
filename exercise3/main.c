afef4dba7c4416bb3a3199053def8b3423b515a9
Bartosz Wajsowicz <bartosz.wajsowicz@tieto.com>
2018-08-09 04:24:13 -0700

    First version of exercise 3

--- exercise3/main.c
+++ exercise3/main.c
@@ -1,13 +1,55 @@
 #include <assert.h>
-
+#include <stdlib.h>
 /**
 * Given a number, return the maximum number that could be formed with digits of the number given.
 * For example: number = 7389, return 9873
 */
+
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)b - *(int*)a );
+}
+
 int form_the_largest_number(int number)
 {
-	return 0;
+    int temp = number;
+    int digit;
+    int lengthOfNumber = 0;
+
+    //gets length of the number
+    while(temp != 0) 
+    {
+        digit = temp % 10;
+        temp = temp / 10;
+        lengthOfNumber++;
+    } 
+
+    int i = 0;
+
+    int* digits = malloc(lengthOfNumber*sizeof(int));
+
+    //puts single digits into array
+    while(number != 0)
+    {
+        digit = number % 10;
+        digits[i] = digit;
+        number = number / 10;
+        i++;
+    }
+
+    //array sort
+    qsort(digits, lengthOfNumber, sizeof(int), cmpfunc);
+
+    int result = 0;
+
+    //build result from sorted array
+    for(i = 0; i < lengthOfNumber; i++)
+    {
+        result = result*10 + digits[i];
+    }
+    
+    return result;
 }
+
 
 void test_cases()
 {
