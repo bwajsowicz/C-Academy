71e7aa47bbba1c5423af6fa10a338c3bad434568
Bartosz Wajsowicz <bartosz.wajsowicz@tieto.com>
2018-08-09 04:47:44 -0700

    First version of exercise 4

--- exercise4/main.c
+++ exercise4/main.c
@@ -1,12 +1,19 @@
 #include <assert.h>
-
+#include <stdio.h>
 /**
 * Function takes a 'start' and 'end' number of a range, and should return the count of all numbers except numbers with 5 in it.
 * For example: start = 4, end = 8. It should return 4, because: 4, 6, 7, 8
 */
 int numbers_without_five(int start, int end)
 {
-	return 0;
+	int i;
+    int count = 0;
+
+    for(i = start; i <= end; i++)
+        if(!((i % 5 == 0)&&(i % 2 != 0))) 
+            count++;
+            
+    return count;
 }
 
 void test_cases()
