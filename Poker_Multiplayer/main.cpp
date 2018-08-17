b87fc2505450d3b9c9ab57fbe64a31d6bf3c2239
Bartosz Wajsowicz <bartosz.wajsowicz@tieto.com>
2018-08-16 22:44:40 -0700

    Poker Multiplayer init.

---
+++ Poker_Multiplayer/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "What is considered a nightmare?" << std::endl;
+    std::cout << "Texas Holdem Multiplayer written in C" << std::endl;
+    return 0;
+}
